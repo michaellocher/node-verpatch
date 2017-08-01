8e92160dc46994dbbda12ea7a5844cbb01a771bf
Michael Locher <locher@hector.de>
2017-08-01 15:10:28 +0200

    first version

---
+++ README.md
@@ -0,0 +1,6 @@
+node-verpatch
+==
+
+This cli tool uses verpatch from (https://www.codeproject.com) to change information of exe files.
+
+Possible values which could be used by the cli are listed here: https://msdn.microsoft.com/de-de/library/system.diagnostics.fileversioninfo.aspx.
---
+++ index.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+
+const path = require('path');
+const exec = require('child_process').exec;
+const verpatch = path.join(__dirname, 'bin', 'verpatch.exe');
+exec(`${verpatch} ${process.argv[2]} /va`, (null, null, console.error))
+.on('exit', () => {
+  exec(`${verpatch} ${`${process.argv[2]} ${process.argv[3]} /pv ${process.argv[3]} ${process.argv.splice(4).map(ar => `/s ${ar.split('=')[0]} "${ar.split('=')[1]}"`).join(' ')}`}`, (null, null, console.error))
+  .on('exit', () => console.log('all successful'));
+});
