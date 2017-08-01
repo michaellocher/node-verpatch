ee0312a64f374dc643e804569e0e7e09a74d1867
Michael Locher <michael_locher@web.de>
2017-08-01 15:29:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,17 @@
 
 const path = require('path');
 const exec = require('child_process').exec;
-const verpatch = path.join(__dirname, '..', 'node-verpatch', 'bin', 'verpatch.exe');
-exec(`${verpatch} ${process.argv[2]} /va`, (null, null, console.error))
+const verpatch = path.join(__dirname, 'bin', 'verpatch.exe');
+exec(`${verpatch} ${process.argv[2]} /va`, (e) => {
+  if (e) {
+    throw new Error(e);
+  }
+})
 .on('exit', () => {
-  exec(`${verpatch} ${`${process.argv[2]} ${process.argv[3]} /pv ${process.argv[3]} ${process.argv.splice(4).map(ar => `/s ${ar.split('=')[0]} "${ar.split('=')[1]}"`).join(' ')}`}`, (null, null, console.error))
+  exec(`${verpatch} ${`${process.argv[2]} ${process.argv[3]} /pv ${process.argv[3]} ${process.argv.splice(4).map(ar => `/s ${ar.split('=')[0]} "${ar.split('=')[1]}"`).join(' ')}`}`, (e) => {
+    if (e) {
+      throw new Error(e);
+    }
+  })
   .on('exit', () => console.log('all successful'));
 });
