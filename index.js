eb33971454d4f3dc54d4989858e96c4a394e5768
Michael Locher <michael_locher@web.de>
2017-08-01 15:22:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 const exec = require('child_process').exec;
-const verpatch = path.join(__dirname, 'bin', 'verpatch.exe');
+const verpatch = path.join(__dirname, '..', 'node-verpatch', 'bin', 'verpatch.exe');
 exec(`${verpatch} ${process.argv[2]} /va`, (null, null, console.error))
 .on('exit', () => {
   exec(`${verpatch} ${`${process.argv[2]} ${process.argv[3]} /pv ${process.argv[3]} ${process.argv.splice(4).map(ar => `/s ${ar.split('=')[0]} "${ar.split('=')[1]}"`).join(' ')}`}`, (null, null, console.error))
