f69e85a5d90237e49a672eb106864404524dfbab
Michael Locher <michael_locher@web.de>
2018-01-29 11:25:36 +0100

    Update index.js
    do not create new versionInfo

--- index.js
+++ index.js
@@ -3,16 +3,9 @@
 const path = require('path');
 const exec = require('child_process').exec;
 const verpatch = path.join(__dirname, 'bin', 'verpatch.exe');
-exec(`${verpatch} ${process.argv[2]} /va`, (e) => {
+exec(`${verpatch} ${`${process.argv[2]} ${process.argv[3]} /pv ${process.argv[3]} ${process.argv.splice(4).map(ar => `/s ${ar.split('=')[0]} "${ar.split('=')[1]}"`).join(' ')}`}`, (e) => {
   if (e) {
     throw new Error(e);
   }
 })
-.on('exit', () => {
-  exec(`${verpatch} ${`${process.argv[2]} /pv ${process.argv[3]} ${process.argv.splice(4).map(ar => `/s ${ar.split('=')[0]} "${ar.split('=')[1]}"`).join(' ')}`}`, (e) => {
-    if (e) {
-      throw new Error(e);
-    }
-  })
-  .on('exit', () => console.log('all successful'));
-});
+.on('exit', () => console.log('all successful'));
