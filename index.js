82c83ab73133dfe725ed65842de7597810b8895c
Michael Locher <michael_locher@web.de>
2018-05-25 13:20:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const verpatch = path.join(__dirname, 'bin', 'verpatch.exe');
 exec(`${verpatch} ${`${process.argv[2]} ${process.argv[3]} /pv ${process.argv[3]} ${process.argv.splice(4).map(ar => `/s ${ar.split('=')[0]} "${ar.split('=')[1]}"`).join(' ')}`}`, (e) => {
   if (e) {
     throw new Error(e);
+    process.exit(1);
   }
 })
 .on('exit', () => console.log('all successful'));
